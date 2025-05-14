64ae060dd1ccf0853ea031b3277bbe81e462804e
ahd-3hd <ahdhani0@gmail.com>
2025-05-14 16:00:21 +0300

    Initial Commit

---
+++ ReproApp/ReproAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReproAppApp.swift
+//  ReproApp
+//
+//  Created by Ahd on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ReproAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
